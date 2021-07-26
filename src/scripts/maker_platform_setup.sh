b2fc2438a682b51c48889cab03ea5ddb69ab2b31
Mark Lee <electronjs@lazymalevolence.com>
2021-07-25 22:19:46 -0700

    [semver:skip] chore: fix shellcheck warnings

--- src/scripts/maker_platform_setup.sh
+++ src/scripts/maker_platform_setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -xe
 
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)"
+
 case "$(uname -s)" in
   "Linux")
     case "$MAKER" in
@@ -11,16 +13,18 @@ case "$(uname -s)" in
         sudo apt-get install --yes --no-install-recommends rpm
         ;;
       "flatpak")
-        "$(dirname $0)"/install_flatpak_dependencies.sh
+        "$DIR"/install_flatpak_dependencies.sh
         ;;
       "snap")
-        "$(dirname $0)"/install_snap_dependencies.sh
+        "$DIR"/install_snap_dependencies.sh
         ;;
     esac
     ;;
   "Windows"|"MINGW"|"MSYS"*)
     if [[ "$MAKER" = "wix" ]]; then
       choco install wixtoolset
+      # WHY: We don't want to expand this, we want this in BASH_ENV verbatim
+      # shellcheck disable=SC2016
       echo 'export PATH="$PATH:/c/Program Files (x86)/WiX Toolset v3.11/bin"' >> "$BASH_ENV"
     fi
     ;;
