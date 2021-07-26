f259c67678649708dd95929b34dc3ba5b74ac3ce
Mark Lee <electronjs@lazymalevolence.com>
2021-07-25 22:46:35 -0700

    [semver:skip] refactor: consolidate scripts

--- src/scripts/maker_platform_setup.sh
+++ src/scripts/maker_platform_setup.sh
@@ -1,6 +1,25 @@
 #!/bin/bash -xe
 
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)"
+install_flatpak_app() {
+    local arch="$1"
+    local app="$2"
+    local branch=$3
+    install_flatpak "app/$app/$arch/$branch"
+}
+
+install_flatpak_runtime() {
+    local arch="$1"
+    local runtime="$2"
+    local version="$3"
+    install_flatpak "runtime/${runtime}/$arch/$version"
+}
+
+install_flatpak() {
+    local ref="$1"
+    if [[ ! -d "$HOME/.local/share/flatpak/$ref" ]]; then
+        flatpak install --user --no-deps --assumeyes "$ref"
+    fi
+}
 
 case "$(uname -s)" in
   "Linux")
@@ -13,10 +32,48 @@ case "$(uname -s)" in
         sudo apt-get install --yes --no-install-recommends rpm
         ;;
       "flatpak")
-        "$DIR"/install_flatpak_dependencies.sh
+        ####
+        # NOTE
+        #
+        # There do not appear to be flatpak app/runtime dependencies for arm64 as of 2021/07/21.
+        # This script has support for it in the future, but for now is likely limited to x86_64.
+        ####
+
+        if [[ -z "$ARCH" ]]; then
+          ARCH=x64
+        fi
+
+        case "$ARCH" in
+          x64)
+            flatpak_arch=x86_64
+            ;;
+          arm64)
+            flatpak_arch=arm64
+            ;;
+          *)
+            echo "ERROR: Unknown arch: '$ARCH'" >&2
+            exit 1
+            ;;
+        esac
+        sudo add-apt-repository -y ppa:alexlarsson/flatpak
+        sudo apt-get update
+        sudo apt-get install -y --no-install-recommends flatpak-builder elfutils
+        flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+        install_flatpak_runtime "$flatpak_arch" org.freedesktop.Sdk 19.08
+        install_flatpak_runtime "$flatpak_arch" org.freedesktop.Platform 19.08
+        install_flatpak_app "$flatpak_arch" org.electronjs.Electron2.BaseApp stable
         ;;
       "snap")
-        "$DIR"/install_snap_dependencies.sh
+        sudo apt-get update
+        sudo apt-get install --yes --no-install-recommends snapcraft
+
+        mkdir -p fakesnap/snap
+        cp ci/snapcraft.yaml fakesnap/snap/
+        pushd fakesnap
+        snapcraft pull desktop-gtk3 electron-deps
+        popd
+        rm -rf fakesnap
         ;;
     esac
     ;;
